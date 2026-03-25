fc73d839ba6e659e20710792e690680273cfe175
Rahul Borana <boranarahul28@gmail.com>
2026-03-26 00:41:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,48 +1,16 @@
 class Solution {
 public:
-
-
-//     🧠 Correct Thinking (Important DP Pattern)
-
-// At every house you have 2 choices:
-
-// 1️⃣ Rob this house
-// → then you cannot rob previous house
-
-// 2️⃣ Skip this house
-// → take previous best
-
-// So:
-
-// dp[i] = max(
-//             nums[i] + dp[i-2],   // rob current
-//             dp[i-1]              // skip current
-//           )
-
-
-
-
     int rob(vector<int>& nums) {
         
-        //code toh dekhlia maine par samjho ki karna kya hai
-        //ADJACENT houses chori nhi kar sakta toh ye kiya hai
+        int prev1 = 0; // dp[i-1]
+        int prev2 = 0; // dp[i-2]
 
-        //TAAKI MAX CHORI HO...
-
-        //used dp tabulation maybe but approach samjho ye imp nahi
-
-        int n=nums.size();
-        vector<int>amt(n);
-
-        if(n==1)return nums[0];
-        amt[0]=nums[0];
-        amt[1]=max(nums[0],nums[1]);
-
-        for(int i=2;i<n;i++){
-            amt[i]=max(amt[i-1],nums[i]+amt[i-2]);
-            //YA toh pichla amount YA toh abhi aur adjacent wali side ka
+        for(int num : nums){
+            int curr = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return amt[n-1];
+        return prev1;
     }
 };
