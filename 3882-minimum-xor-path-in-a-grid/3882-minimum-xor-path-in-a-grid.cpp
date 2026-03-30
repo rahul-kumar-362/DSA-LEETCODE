551dc849756360f3523cd2a7eee2dba094e50bd4
Rahul Borana <boranarahul28@gmail.com>
2026-03-30 16:53:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
@@ -1,57 +1,37 @@
 class Solution {
 public:
-
-    //int dp[1001][1001][1024];
-    vector<vector<vector<int>>>dp;
-
-    int solve(int i,int j,vector<vector<int>>& grid,int m,int n,int currXor){
-        //right ke liye aur down ke liye
-
-        //💡 Missing concept:
-//👉 Har step pe XOR carry karna padega
-
-
-    
-
-        if(i>=m || j>=n){//bade hue toh break
-            
-            return INT_MAX;//pata nahi chut  
-        }
-
-        currXor^=grid[i][j];//curr Cell include karo
-
-
-        if(i==m-1 && j==n-1){//pohoch gaye toh kya kare
-            return  currXor;
-        }
-
-        if(dp[i][j][currXor]!=-1){//2D leta toh chud jaata
-        return dp[i][j][currXor];
-    }
-        int right=solve(i+1,j,grid,m,n,currXor);
-        int down=solve(i,j+1,grid,m,n,currXor);
-        
-        return dp[i][j][currXor] = min(right,down);
-        
-    }
     int minCost(vector<vector<int>>& grid) {
-        //brute force
+        int m = grid.size();
+        int n = grid[0].size();
 
-        //since initial is (0,0);
-      //  int pt=grid[0][0];//grid of 0,0
+        vector<vector<unordered_set<int>>> dp(m, vector<unordered_set<int>>(n));
 
-           
-        int m=grid.size();
-        int n=grid[0].size();
+        dp[0][0].insert(grid[0][0]);
 
-        
-        // ✅ proper initialization
-        dp = vector<vector<vector<int>>>(m, 
-             vector<vector<int>>(n, vector<int>(1024, -1)));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
 
+                if(i == 0 && j == 0) continue;
 
-       
-        return solve(0,0,grid,m,n,0);
-        
+                if(i > 0){
+                    for(int val : dp[i-1][j]){
+                        dp[i][j].insert(val ^ grid[i][j]);
+                    }
+                }
+
+                if(j > 0){
+                    for(int val : dp[i][j-1]){
+                        dp[i][j].insert(val ^ grid[i][j]);
+                    }
+                }
+            }
+        }
+
+        int ans = INT_MAX;
+        for(int val : dp[m-1][n-1]){
+            ans = min(ans, val);
+        }
+
+        return ans;
     }
 };
