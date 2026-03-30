97de16c32f28d2e340f24dc5b41a6b2614a33bb8
Rahul Borana <boranarahul28@gmail.com>
2026-03-30 19:44:32 +0530

    Time: 3 ms (27.36%), Space: 16.9 MB (53.34%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,47 +1,59 @@
 class Solution {
 public:
+    vector<vector<int>> d{{-1,0}, {1,0},{0,-1}, {0,1}};
+    typedef pair<int,int> P;
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        queue<pair<int,int>> q;
+        //we wiil use BFS method to simultanoiusly cover all the rotten at once affecting their adjacent oranges 
+
+        //for bfs we use Queue data structure
+        queue<P> q;
+
+
         int fresh = 0;
 
-        // scan
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2) q.push({i,j});
-                else if(grid[i][j]==1) fresh++;
-            }
-        }
-
-        if(fresh == 0) return 0; // 🔥 early exit
-
-        int dx[4] = {1,0,-1,0};
-        int dy[4] = {0,1,0,-1};
-
-        int minutes = -1;
-
-        while(!q.empty()){
-            int sz = q.size();
-            minutes++;
-
-            while(sz--){
-                auto [x,y] = q.front();
-                q.pop();
-
-                for(int k=0;k<4;k++){
-                    int nx = x + dx[k];
-                    int ny = y + dy[k];
-
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1){
-                        grid[nx][ny] = 2;
-                        q.push({nx,ny});
-                        fresh--;
-                    }
+        //in this for loop we will stre the coords where there is rotten orange annd also count the no of fresh ones in grid
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(grid[i][j]==2) {
+                    q.push({i,j});
+                }
+                if(grid[i][j]==1) {
+                    fresh++;
                 }
             }
         }
 
-        return (fresh==0 ? minutes : -1);
-    }
+        //applying bfs on the queue for all the coords wwe will check for the neighbout fresh ones and stpre in the queue if not rotten and then rot them and also increment the minute when the queue gets empty for that one round 
+        int minutes = 0;
+        while(!q.empty()) {
+            int N = q.size();
+            bool flag = false;
+            while(N--) {
+                
+                P front = q.front();
+                q.pop();
+                int i = front.first;
+                int j = front.second;
+                for(int k = 0; k<4; k++) {
+                    int new_i = i + d[k][0];
+                    int new_j = j + d[k][1];
+
+                    if(new_i>=0 && new_j>=0 && new_i<m && new_j<n) {
+                        if(grid[new_i][new_j]==1) {
+                            flag  = true;
+                            grid[new_i][new_j] = 2;
+                            fresh--;
+                            q.push({new_i,new_j});
+                        }
+                    }
+                }
+            }
+            if(flag==true) minutes++;
+        }
+        cout<<fresh<<" "<<minutes;
+        return (fresh==0) ? minutes : -1 ;
+     }
 };
