4ba193fcc4e68d3418d12bcda76ac50ca7d3d2d9
Rahul Borana <boranarahul28@gmail.com>
2026-04-02 00:46:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
         //first lets try the Linear Search ALGO
 
         int m=matrix.size();
         int n=matrix[0].size();
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+        int lo=0;
+        int hi=m*n-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int currow=mid/n;
+            int currcol=mid%n;
+            if(matrix[currow][currcol]==target) return true;
+            else if(matrix[currow][currcol]<target)lo=mid+1;
+            else hi=mid-1;
         }
         return false;
     }
