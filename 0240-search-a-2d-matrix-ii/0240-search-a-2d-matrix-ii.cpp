3d84def531a0ad30888b77ed0cf983394aed4196
Rahul Borana <boranarahul28@gmail.com>
2026-04-02 00:42:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -6,12 +6,15 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+        int lo=0;
+        int hi=m*n-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            int currow=mid/n;
+            int currcol=mid%n;
+            if(matrix[currow][currcol]==target) return true;
+            else if(matrix[currow][currcol]<target)lo=mid+1;
+            else hi=mid-1;
         }
         return false;
     }
