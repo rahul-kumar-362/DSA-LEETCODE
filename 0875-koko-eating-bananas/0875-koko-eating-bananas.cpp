99f50d157df105fd7561b124549a9ef0c6b3ab9e
Rahul Borana <boranarahul28@gmail.com>
2026-04-01 19:09:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,14 +1,27 @@
+     
+        //💀 FINAL INSIGHT
+
+// Binary search tab lagta hai jab answer space me
+// “first true” ya “first valid” find karna ho
+
+//🔥
+
+
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         long long hi=*max_element(piles.begin(),piles.end());
 
         long long lo=1;
-        
-        //💀 FINAL INSIGHT
+   
 
-// Binary search tab lagta hai jab answer space me
-// “first true” ya “first valid” find karna ho
+// 🧠 Statement kya bolta hai?
+
+// “Agar pile me k se kam bananas hai, toh Koko usse 1 hour me finish karegi”
+
+// 📦 Matlab simple language me
+
+// 👉 Koko har hour max k bananas kha sakti hai
 
         while(lo<=hi){
 
@@ -16,7 +29,7 @@ public:
             long long time=0;
 
             for(int  i=0;i<piles.size();i++){
-                time+=(piles[i]+mid-1)/mid;
+                time+=(piles[i]+mid-1)/mid; //isse ceil(pile/mid) Nikalta hai
             }
             if(time<=h){
                 hi=mid-1;
