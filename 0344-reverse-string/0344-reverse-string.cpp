180c6386bca15db263c9d146918936d9ea52dd94
Rahul Borana <boranarahul28@gmail.com>
2026-04-15 01:56:44 +0530

    Time: 0 ms (100%), Space: 27.2 MB (80.34%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     void solve(vector<char>& s,int i){
-        if(i>s.size()/2)return ;
+        if(i>=s.size()/2)return ;
         swap(s[i],s[s.size()-i-1]);
         solve(s,i+1);
     }
