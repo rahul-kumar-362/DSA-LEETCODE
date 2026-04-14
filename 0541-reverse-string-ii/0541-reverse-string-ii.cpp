5f4e0c887c6f171770b7d96cf480e5d6ab0e8390
Rahul Borana <boranarahul28@gmail.com>
2026-04-15 02:11:06 +0530

    Time: 0 ms (100%), Space: 9.6 MB (46.2%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+
+        for (int i = 0; i < n; i += 2 * k) {
+            // If there are at least k characters left, reverse k.
+            // Otherwise, reverse whatever is left (n - i characters).
+            if (i + k <= n) {
+                reverse(s.begin() + i, s.begin() + i + k);
+            } else {
+                reverse(s.begin() + i, s.end());
+            }
+        }
+       
+        return s;
+    }
+};
