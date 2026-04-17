c0a4fffdddc250668e48b35ac9d6ce37d32c86c7
Rahul Borana <boranarahul28@gmail.com>
2026-04-17 23:37:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -12,20 +12,28 @@ public:
 
 //T.C and S.C === O(N)
 
-    int solve(int n,vector<int>&dp){
-        if(n==1 || n==0){
-            return n;
-        }
-        if(dp[n]!=-1){ //if already calculated ...
-            return dp[n];
-        }
+    // int solve(int n,vector<int>&dp){
+    //     if(n==1 || n==0){
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){ //if already calculated ...
+    //         return dp[n];
+    //     }
 
 
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
+    //     return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    // }
     int fib(int n){
-        //use memoization
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        //use Tabulation
+
+       // if(n==0)return 0;
+        vector<int>dp(n+1,0);
+
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
