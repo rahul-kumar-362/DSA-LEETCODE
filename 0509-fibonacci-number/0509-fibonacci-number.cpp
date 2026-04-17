484d4e7188e7852ede4dec409ab240b08ade25e6
Rahul Borana <boranarahul28@gmail.com>
2026-04-17 23:16:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
-    int fib(int n) {
+    // int fib(int n) {
+    //     if(n==1 || n==0){
+    //         return n;
+    //     }
+    //     return fib(n-1)+fib(n-2);
+    // }
+    //KYA CHHEDA BHOSADI ---baar baar kyu likhrea hai
+
+
+
+
+    int solve(int n,unordered_map<int,int>&dp){
         if(n==1 || n==0){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp.count(n)){ //if already calculated ...
+            return dp[n];
+        }
+
+
+        return dp[n]=fib(n-1)+fib(n-2);
+    }
+    int fib(int n){
+        //use memoization
+        unordered_map<int,int>dp;
+        return solve(n,dp);
     }
 };
