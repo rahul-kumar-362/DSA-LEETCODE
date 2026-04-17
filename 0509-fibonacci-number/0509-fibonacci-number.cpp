429edccc77ef69b50c5b5171c1d998a849fd5a3e
Rahul Borana <boranarahul28@gmail.com>
2026-04-17 23:46:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -44,13 +44,12 @@ public:
         if(n==0)return 0;
         int prev2=0;
         int prev1=1;
-        int current=prev1+prev2;
         for(int i=2;i<=n;i++){
 
-            current=prev1+prev2;
+            int current=prev1+prev2;
             prev2=prev1;
             prev1=current;
         }
-        return current;
+        return prev1;
     }
 };
