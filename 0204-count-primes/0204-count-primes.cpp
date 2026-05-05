1adb847bd9e625ebc2f3998b1b26273f283bccea
Rahul Borana <boranarahul28@gmail.com>
2026-05-05 20:02:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,26 +1,20 @@
 class Solution {
-    
 public:
     int countPrimes(int n) {
+        if(n <= 2) return 0;
 
-        vector<bool> prime(n+1,true);
-        prime[0]=prime[1]=false;
-        for(int i=2;i*i<n;i++)
-        {
-            if(prime[i])
-            {
-               // count++;
-                for(int j=i*i;j<n;j=j+i)
-                {
-                    prime[j]=false;
+        int count = n / 2; // assume all odds are prime
+        vector<bool> prime(n, true);
+
+        for(int i = 3; i * i < n; i += 2) {
+            if(prime[i]) {
+                for(int j = i * i; j < n; j += 2 * i) {
+                    if(prime[j]) {
+                        prime[j] = false;
+                        count--;
+                    }
                 }
             }
-        }
-
-        int count = 0;
-        for(int i=2;i<n;i++)
-        {
-            if(prime[i]) count++;
         }
 
         return count;
