3de509ecfab16bfa18626285a84ebf4e755426db
Rahul Borana <boranarahul28@gmail.com>
2026-06-14 10:48:28 +0530

    Time: 1 ms (2.09%), Space: 10.3 MB (34.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -9,10 +9,10 @@ public:
         //Brute Force Approach ...
         vector<vector<int>>ans;
 
-        for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
             vector<int>temp;
-            for(int j=n-1;j>=0;j--){
-                temp.push_back(matrix[j][i]);
+            for(int i=n-1;i>=0;i--){
+                temp.push_back(matrix[i][j]);
             }
             ans.push_back(temp);
         }
