fd977c12851fb9d29f5767fb0b5fc4a8aa60a459
Rahul Borana <boranarahul28@gmail.com>
2026-06-15 11:57:14 +0530

    Time: 0 ms (100%), Space: 15.6 MB (92.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
+
+
+    //Optimal APPROACH ...
+
+
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+
+        int n=nums.size();
+
+        int idx=-1;//kept index 
+
+
+
+        //STEP 1 :  TO FIND THE IDX  OR BREAKPOINT/DIP 
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){//agar decreasing sort order break kiya toh
+                idx=i;//got idx
+                break;
+            }
+        }
+
+        if(idx==-1){//iska matlab reverse sorted hai array
+        //therefore,ans will be sorted sequence 
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        //STEP 2 : NOW to swap with the smallest one but greater than idx
+
+        for(int i=n-1;i>=idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+
+        //STEP 3 : Hence got the first corrected sequence now WANT other Sorted sequence for the MINIMUM
+
+        //therefore
+
+
+        reverse(nums.begin()+idx+1,nums.end());
+
     }
 };
