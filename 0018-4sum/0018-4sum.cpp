79bcfa5a06082400a620b1abacd0e7be0e340d3f
Rahul Borana <boranarahul28@gmail.com>
2026-06-16 12:47:16 +0530

    Time: 1588 ms (5%), Space: 507.6 MB (5.02%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        //complete brute force approach //Approx T.C. - O(n^4)
+        //BETTER APPROACH USING HASHING
 
 
-        sort(nums.begin(),nums.end());
-        
-        set<vector<int>>s;
         int n=nums.size();
+        set<vector<int>>uniQuetriplets;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-    if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
 
-    for(int j=i+1;j<n;j++){
-        if(j>i+1 && nums[j]==nums[j-1]) continue;
+                set<long long >s;
+                for(int k=j+1;k<n;k++){
+                    long long fourth = 1LL*target - nums[i] - nums[j] - nums[k];
 
-        for(int k=j+1;k<n;k++){
-            if(k>j+1 && nums[k]==nums[k-1]) continue;
+                    if(s.count(fourth)){//agar mil gaya
+                        //store answer
 
-            for(int l=k+1;l<n;l++){
-                if(l>k+1 && nums[l]==nums[l-1]) continue;
+                        vector<int>temp = {nums[i],nums[j],nums[k],(int)fourth};
+                        uniQuetriplets.insert(temp);
+                    }
 
-                long long sum = 1LL*nums[i] + nums[j] + nums[k] + nums[l];
-
-                if(sum == target){
-                    s.insert({nums[i], nums[j], nums[k], nums[l]});
+                    s.insert(nums[k]);
                 }
             }
         }
-    }
-}
-        vector<vector<int>>ans(s.begin(),s.end());
-        return ans;
+        return vector<vector<int>>(uniQuetriplets.begin(),uniQuetriplets.end());
     }
 };
