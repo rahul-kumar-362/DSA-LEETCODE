8bd16044ebfb4a9a0a5c29da88f91fff6536df45
Rahul Borana <boranarahul28@gmail.com>
2026-06-21 14:39:04 +0530

    Time: 3 ms (70.07%), Space: 15.5 MB (70.13%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+
+    void solve(int open,int close,int n ,vector<string>&ans ,string temp){
+
+
+        //ohh bc agar open aur close dono n tab return ;
+
+        if(temp.size()==2*n){//jab ssize reach tab pushback
+            ans.push_back(temp);
+            return;
+        }
+
+        if(open<n){
+            solve(open+1,close,n,ans,temp+"(");
+        }
+        if(close<open){
+            solve(open,close+1,n,ans,temp+")");
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        //to generate parentthesis we will use recursion with stack
+
+        string temp="";
+        vector<string>ans;
+        solve(0,0,n,ans,"");
+        return ans;
+    }
+};
