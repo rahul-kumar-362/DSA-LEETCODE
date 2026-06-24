3d75f5191f8ed01af8398ec0046e3295b9302694
Rahul Borana <boranarahul28@gmail.com>
2026-06-24 21:20:57 +0530

    Time: 8 ms (32.07%), Space: 14.1 MB (40.83%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        //brute force....
+        vector<string>ans(numRows,"");//initially
+
+        int n=s.size();
+        if(numRows==1)return s; // ye hai jab no zigzag
+
+        int r=0;
+        int tot=0;
+        while(tot<n){
+            // int idx=i%(numRows);
+            // ans[idx]+=s[r];
+            while(tot<n && r<numRows){
+                ans[r]+=s[tot];
+                r++;
+                tot++;
+            }
+            r-=2;//kyuki loop khatam hone par 2 plus
+            while(tot<n && r!=0){
+                ans[r]+=s[tot];
+                r--;
+                tot++;
+            }
+        }
+        string result="";
+        for(int i=0;i<ans.size();i++){
+            result+=ans[i];
+        }
+        return result;
+    }
+};
