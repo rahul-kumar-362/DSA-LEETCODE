2058494795306c012b2ed9e80ed66f61850d33af
Rahul Borana <boranarahul28@gmail.com>
2026-06-27 02:29:34 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.93%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,10 +1,40 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        long long res=1LL*dividend/divisor;  //BAKWAS SOLUTION...
+        if(dividend==divisor)return 1;
 
-        if(res>INT_MAX)return INT_MAX;
-        else if(res<INT_MIN)return INT_MIN;
-        return (int)res;
+        bool sign = true;//choose +ve initially
+
+        if(dividend>=0 && divisor<0)sign=false;//case 1: for -ve
+        else if(dividend<0 && divisor>=0)sign=false;
+
+        long n= abs((long)dividend);
+        long d = abs((long)divisor);
+
+        long quotient = 0;
+
+
+        while(n>=d){//jabtak numerator denominator ke eQual ya Bada ho ...logn time
+            int count=0;
+            while(n>=(d<<count+1)){//jabtak n bada ho d*2^count+1 se tabtak
+                count++;
+            }
+            quotient += 1<<count;
+            n -= d<<count; 
+        }
+
+        if(quotient == 1<<31 && sign){
+            return  INT_MAX;
+        }
+
+        
+        if(quotient == 1<<31 && !sign){
+            return  INT_MIN;
+        }
+
+
+        return sign ? quotient : -quotient;//+ve toh + warna -
+
+
     }
 };
