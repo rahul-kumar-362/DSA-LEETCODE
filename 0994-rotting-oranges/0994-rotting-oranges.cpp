ab1bc2a267dee1f9adfe2675cd32a17cc15b41e7
Rahul Borana <boranarahul28@gmail.com>
2026-07-02 00:57:59 +0530

    Time: 0 ms (100%), Space: 16.7 MB (70.16%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,59 +1,75 @@
 class Solution {
 public:
-    vector<vector<int>> d{{-1,0}, {1,0},{0,-1}, {0,1}};
-    typedef pair<int,int> P;
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
 
-        //we wiil use BFS method to simultanoiusly cover all the rotten at once affecting their adjacent oranges 
+        int n = grid.size();
+        int m = grid[0].size();
 
-        //for bfs we use Queue data structure
-        queue<P> q;
-
-
+        queue<pair<int,int>> q;
         int fresh = 0;
 
-        //in this for loop we will stre the coords where there is rotten orange annd also count the no of fresh ones in grid
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(grid[i][j]==2) {
+        // 4 directions
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
+
+        // Step 1: Put all rotten oranges into queue
+        //         Count fresh oranges
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                if(grid[i][j]==1) {
+
+                else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
 
-        //applying bfs on the queue for all the coords wwe will check for the neighbout fresh ones and stpre in the queue if not rotten and then rot them and also increment the minute when the queue gets empty for that one round 
-        int minutes = 0;
-        while(!q.empty()) {
-            int N = q.size();
-            bool flag = false;
-            while(N--) {
-                
-                P front = q.front();
-                q.pop();
-                int i = front.first;
-                int j = front.second;
-                for(int k = 0; k<4; k++) {
-                    int new_i = i + d[k][0];
-                    int new_j = j + d[k][1];
+        // No fresh oranges
+        if(fresh==0) return 0;
 
-                    if(new_i>=0 && new_j>=0 && new_i<m && new_j<n) {
-                        if(grid[new_i][new_j]==1) {
-                            flag  = true;
-                            grid[new_i][new_j] = 2;
-                            fresh--;
-                            q.push({new_i,new_j});
-                        }
+        int minutes = 0;
+
+        // Step 2: BFS
+        while(!q.empty()){
+
+            int sz = q.size();
+
+            // Process one level
+            while(sz--){
+
+                auto front = q.front();
+                q.pop();
+
+                int row = front.first;
+                int col = front.second;
+
+                for(int k=0;k<4;k++){
+
+                    int nr = row + dx[k];
+                    int nc = col + dy[k];
+
+                    if(nr>=0 && nr<n &&
+                       nc>=0 && nc<m &&
+                       grid[nr][nc]==1){
+
+                        grid[nr][nc]=2;
+                        fresh--;
+
+                        q.push({nr,nc});
                     }
                 }
             }
-            if(flag==true) minutes++;
+
+            // Increase time only if new oranges were added
+            if(!q.empty())
+                minutes++;
         }
-        cout<<fresh<<" "<<minutes;
-        return (fresh==0) ? minutes : -1 ;
-     }
+
+        if(fresh>0) return -1;
+
+        return minutes;
+    }
 };
