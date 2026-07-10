09c0cb55b21e146574ecfb261e5215b64e51276a
Rahul Borana <boranarahul28@gmail.com>
2026-07-11 00:27:27 +0530

    Time: 69 ms (19.5%), Space: 44 MB (36.18%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -20,58 +20,27 @@
 
 class Solution {
 public:
-    //BRUTE FORCE...O(N^2)
-    bool isValidSubtree(TreeNode* root,TreeNode* p, TreeNode* q,bool &l,bool &r){
-        //check iss subtree mai dono hai kya
-
-        if(root == NULL)return false;
-
-        if(root == p){
-            l = true;
-        }
-        else if(root == q){
-            r = true;
-        }
-        else if(l && r){//agar dono true ... toh //recursion se pehle kaise...
-            return true;
-        }
-        isValidSubtree(root->left,p,q,l,r);
-        isValidSubtree(root->right,p,q,l,r);
-        return l&&r;//recursion ke baad CHECK
-    }
-
-
-
-
-    //     Intuition
-    // Agar current subtree me p aur q dono nahi hain → NULL
-    // Agar hain → pehle dekh left me aur chhota valid subtree hai kya.
-    // Fir right me.
-    // Agar kahin aur nahi mila → current node hi LCA.
-    TreeNode* solve(TreeNode*root,TreeNode* p, TreeNode* q){
-        //temp = root inintially
+   
+    TreeNode* solve(TreeNode*root,TreeNode* p, TreeNode* q){ //OPTIMAL...T.C.->O(N)
 
         if(root == NULL)return NULL;
-        
-        bool l = false;
-        bool r = false;
 
+        if(root ==p || root ==q)return root;//checking for getting LCA
 
-        if(isValidSubtree(root,p,q,l,r)){
-            TreeNode* left = solve(root->left,p,q);
-            if(left) return left;
+            TreeNode* left = solve(root->left,p,q);//check for left
 
-            TreeNode* right = solve(root->right,p,q);
-            if(right) return right;
+            TreeNode* right = solve(root->right,p,q);//check for right
+           
 
-            return root;
-        }
-        return NULL;
+            if( left!= NULL && right!=NULL)return root;
+            else if(left)return left;
+            else if(right)return right;
+            return NULL;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         //jispar dono return wahi lowest parent fir return 
 
-        if(p->val==100000 && q->val==99999)return q;
+
         return solve(root,p,q);
     }
 };
