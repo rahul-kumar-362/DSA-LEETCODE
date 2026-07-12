20005529d07b2d7291603330289f7a93107e00d1
Rahul Borana <boranarahul28@gmail.com>
2026-07-12 11:49:30 +0530

    Time: 3 ms (50.53%), Space: 17.8 MB (97.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,66 +1,34 @@
 class Solution {
 public:
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-        vector<int>ans;
-        //SORT BY FREQUENCY
-        unordered_map<int,int>mpp;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
+        //SAME AS MAX_HEAP  but using MIN_HEAP
+        unordered_map<int, int> mpp;
+
+        for (int x : nums)
+            mpp[x]++;
+
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            greater<pair<int,int>>
+        > pq;
+
+        for (auto &it : mpp) {
+
+            pq.push({it.second, it.first});
+
+            if (pq.size() > k)
+                pq.pop();
         }
 
-        
+        vector<int> ans;
 
-        // priority_queue<pair<int,int>>p;
-        // for(auto &it:mpp){
-        //     p.push({it.second,it.first});
-        // }
-        //         Current
-
-        // m insertions
-
-        // O(m log m)
-
-        // Optimized
-
-        // Build vector : O(m)
-
-        // Heapify : O(m)
-
-  
-
-        vector<pair<int,int>>p;
-        for(auto &it : mpp){
-            p.push_back({it.second, it.first});
-        }
-
-        priority_queue<pair<int,int>>pq(p.begin(),p.end());//banado pq O(m) mai...
-
-        for(int i=0;i<k;i++){
-          ans.push_back(pq.top().second);
-          pq.pop();
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
 
         return ans;
-
-        // Time Complexity:
-// Frequency Map      -> O(n)
-// Vector Creation    -> O(m)
-// Heapify            -> O(m)
-// Extract Top k      -> O(k log m)
-// Total              -> O(n + m + k log m)
-// Worst Case (m = n) -> O(n + k log n)
-
-// Space Complexity:
-// unordered_map      -> O(m)
-// vector<pair>       -> O(m)
-// priority_queue     -> O(m)
-// answer vector      -> O(k)
-// Total              -> O(m + k)
-// Worst Case         -> O(n + k)
-
-//m = number of unique elements.
     }
 };
