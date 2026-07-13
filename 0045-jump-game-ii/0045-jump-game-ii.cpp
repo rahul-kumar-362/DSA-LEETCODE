2c156420211e8b3b15db91be57f95874025d4240
Rahul Borana <boranarahul28@gmail.com>
2026-07-13 21:31:35 +0530

    Time: 0 ms (100%), Space: 20.4 MB (84.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,63 +1,26 @@
-// MEMOIZATION (Top-Down DP)
-
-// IDEA:
-// solve(i) = Minimum jumps required to reach the last index
-// starting from index i.
-//
-// From the current index, try all possible jumps and take
-// the minimum among them.
-
-// Recurrence:
-// solve(i) = 1 + min( solve(i+1), solve(i+2), ... , solve(i+nums[i]) )
-
-// Base Case:
-// If we are already at the last index,
-// then 0 more jumps are required.
-
-// Time Complexity:
-// O(n²)
-// Each index is computed only once, and from each index we
-// may try at most O(n) possible jumps.
-
-// Space Complexity:
-// O(n)
-// O(n) DP array + O(n) recursion stack.
-
 class Solution {
 public:
 
-    int solve(vector<int>& nums, int i, vector<int>& dp) {
-
-        // Reached the last index
-        if(i == nums.size() - 1)
-            return 0;
-
-        // Already computed
-        if(dp[i] != -1)
-            return dp[i];
-
-        int ans = INT_MAX;
-
-        int end = min(i + nums[i], (int)nums.size() - 1);
-
-        // Try every possible jump
-        for(int j = i + 1; j <= end; j++) {
-
-            int temp = solve(nums, j, dp);
-
-            if(temp != INT_MAX)
-                ans = min(ans, 1 + temp);
-        }
-
-        return dp[i] = ans;
-    }
-
+    //MOST OPTIMAL O(N) - GREEDY APPROACH SOLUTION ....
     int jump(vector<int>& nums) {
-
         int n = nums.size();
+        if(n==1)return 0;
+        int jumps = 1;//KYUKI 1 JUMP ALREADY LE CHUKE HAI...
+        int L = 1;
+        int R = nums[0];
+        int temp=0;
 
-        vector<int> dp(n, -1);
-
-        return solve(nums, 0, dp);
+        while(R<n-1){
+            while(L<=R){  
+                temp = max(temp,L+nums[L]);
+                L++;
+            }
+         
+            L=R+1;
+            //R=nums[R];//aage
+            R = temp;
+        jumps++; 
+        }
+        return jumps;
     }
 };
