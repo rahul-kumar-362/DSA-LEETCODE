7b22c65f787020d4913a5ba8d99b05f3bb71c2ac
Rahul Borana <boranarahul28@gmail.com>
2026-07-16 08:41:42 +0530

    Time: 0 ms (100%), Space: 20.6 MB (46.31%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
 
-    // MOST OPTIMAL O(N) - GREEDY APPROACH
+    //MOST OPTIMAL O(N) - GREEDY APPROACH SOLUTION ....
     int jump(vector<int>& nums) {
-
         int n = nums.size();
-        if(n == 1) return 0;
+        if(n==1)return 0;
+        int jumps = 1;//KYUKI 1 JUMP ALREADY LE CHUKE HAI...
+        int L = 1;
+        int R = nums[0];
+        int temp=0;
 
-        int jumps = 0;
-        int L = 0;      // Current index
-        int R = 0;      // Current jump ki boundary
-        int temp = 0;   // Farthest index jo ab tak reach kar sakte hain
-
-        while(L < n - 1) {
-
-            // Current position se sabse door tak pahunch sakte hain
-            temp = max(temp, L + nums[L]);
-
-            // Agar current jump ki boundary par aa gaye
-            if(L == R) {
-                jumps++;
-                R = temp;
+        while(R<n-1){
+            while(L<=R){  
+                temp = max(temp,L+nums[L]);
+                L++;
             }
-
-            L++;
+         
+            L=R+1;
+            //R=nums[R];//aage
+            R = temp;
+        jumps++; 
         }
-
         return jumps;
     }
 };
