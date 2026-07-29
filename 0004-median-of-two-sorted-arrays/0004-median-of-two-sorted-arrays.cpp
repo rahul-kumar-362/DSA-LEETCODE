d3f8429796365749234e113da44a160b7e0e7ad6
Rahul Borana <boranarahul28@gmail.com>
2026-07-29 16:27:17 +0530

    Time: 0 ms (100%), Space: 95.1 MB (62.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,27 +1,70 @@
 class Solution {
 public:
-    //BRUTE FORCE ...
+    //better solution ...
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
         int n1 = nums1.size();
         int n2 = nums2.size();
         int n = n1+n2;
+        int i = 0;
+        int j = 0;
+        int prev = 0;
+        int curr = 0;
 
-        //if(n==even ) if(n==odd)
-        vector<int>arr;
-        for(int i=0;i<n1;i++){
-            arr.push_back(nums1[i]);
-        }
-        for(int i=0;i<n2;i++){
-            arr.push_back(nums2[i]);
+        while( i<n1 && j<n2 ){
+            if(nums1[i]<nums2[j]){
+                curr = nums1[i];
+                i++;
+            }
+            else{
+                curr = nums2[j];
+                j++;
+            }
+            if(n%2!=0 ){//odd
+                if(i+j==n/2+1)return curr;
+            }
+
+            else{//EVEN
+                if(i+j == n/2+1){
+                    return (prev+curr)/2.0;
+                }
+            }
+
+            prev = curr;//storing current to previous 
         }
 
-        sort(arr.begin(),arr.end());
+        while(i<n1){
+            curr = nums1[i];
+            i++;
 
-        if(n%2!=0){
-            return arr[n/2];
+            if(n%2!=0 ){//odd
+                if(i+j==n/2+1)return curr;
+            }
+
+            else{//EVEN
+                if(i+j == n/2+1){
+                    return (prev+curr)/2.0;
+                }
+            }
+
+            prev = curr;//storing current to previous 
         }
-        else{
-           return (arr[n/2]+arr[n/2-1])/2.0;
+        while(j<n2){
+
+            curr = nums2[j];
+            j++;
+
+            if(n%2!=0 ){//odd
+                if(i+j==n/2+1)return curr;
+            }
+
+            else{//EVEN
+                if(i+j == n/2+1){
+                    return (prev+curr)/2.0;
+                }
+            }
+
+            prev = curr;//storing current to previous 
         }
         return -1;
     }
