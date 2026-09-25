6a6eb9e5fc83c8ee47823d7beba09d2e00833fd1
Rahul Borana <boranarahul28@gmail.com>
2026-09-26 01:22:32 +0530

    Time: 336 ms (93.22%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,21 +1,11 @@
 SELECT
-    E.id,
-    COALESCE(O.student, E.student) AS student
-FROM Seat AS E
-LEFT JOIN Seat AS O
-ON (
-      (E.id % 2 = 1 AND O.id = E.id + 1)
-   OR (E.id % 2 = 0 AND O.id = E.id - 1)
-)
-ORDER BY E.id;
-
-
--- COALESCE(a, b)
-
--- = Agar a NULL hai to b return karo.
-
--- Most common use:
-
--- LEFT JOIN ke baad NULL handle karna.
-
--- Default value dena.
+    CASE
+        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat)
+            THEN id + 1
+        WHEN id % 2 = 0
+            THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
